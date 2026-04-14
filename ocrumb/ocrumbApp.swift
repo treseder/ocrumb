9db2700d44b716de1aa46f7da847840c697d868a
plumcode <info@plumcode.com>
2026-04-13 21:58:19 -0600

    Initial Commit

---
+++ ocrumb/ocrumbApp.swift
@@ -0,0 +1,17 @@
+//
+//  ocrumbApp.swift
+//  ocrumb
+//
+//  Created by Travis Treseder on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct ocrumbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
